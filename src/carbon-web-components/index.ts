f56cab2c3938de56430ea10436df08be4057e54b
siaikin <abc1310054026@163.com>
2023-11-05 17:40:03 +0800

    refactor: 修改 carbon 导入文件结构

--- src/carbon-web-components/index.ts
+++ src/carbon-web-components/index.ts
@@ -1,2 +1,16 @@
-import "./carbon-web-components";
-import "./carbon-web-components.scss";
+import "./styles.scss";
+
+import "@carbon/web-components/es/index.js";
+import "@carbon/web-components/es/components/select/index.js";
+import "@carbon/web-components/es/components/ui-shell/index.js";
+import "@carbon/web-components/es/components/form/index.js";
+import "@carbon/web-components/es/components/modal/index.js";
+
+declare module "solid-js" {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      [key: string]: unknown;
+    }
+  }
+}
